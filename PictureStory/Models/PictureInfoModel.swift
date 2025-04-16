b2ce56662b7ca6295f7cac16189423d06b483c72
Hannu Packalen <hpackalen@lcs.on.ca>
2025-04-16 09:43:01 -0400

    Added a file and model for picture info

---
+++ PictureStory/Models/PictureInfoModel.swift
@@ -0,0 +1,17 @@
+//
+//  PictureInfoModel.swift
+//  PictureStory
+//
+//  Created by Hannu Packalen on 2025-04-16.
+//
+
+import Foundation
+import SwiftUICore
+
+struct PictureInfo {
+    let Picture: Image
+    let When: Date
+    let Where: String
+    let Who: String
+    let Story: String
+}
