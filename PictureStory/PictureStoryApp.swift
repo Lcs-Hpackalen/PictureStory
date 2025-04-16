f499a7b4322bda2851c9ad59f96ed4f28f06f1dd
Hannu Packalen <hpackalen@lcs.on.ca>
2025-04-16 09:36:35 -0400

    Initial Commit

---
+++ PictureStory/PictureStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictureStoryApp.swift
+//  PictureStory
+//
+//  Created by Hannu Packalen on 2025-04-16.
+//
+
+import SwiftUI
+
+@main
+struct PictureStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
