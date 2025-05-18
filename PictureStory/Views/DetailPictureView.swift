11804f6c9e3724c1fa7819f8e7f6b42baba90d93
Hannu Packalen <hpackalen@lcs.on.ca>
2025-05-17 20:13:40 -0400

    Added a file for the detail view of pictures.

---
+++ PictureStory/Views/DetailPictureView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailPictureView.swift
+//  PictureStory
+//
+//  Created by Hannu Packalen on 2025-05-17.
+//
+
+import SwiftUI
+
+struct DetailPictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailPictureView()
+}
