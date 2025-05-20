4793f77c77e43d838f0b786db3a6d227306d2c9e
Hannu Packalen <hpackalen@lcs.on.ca>
2025-05-20 11:42:05 -0400

    Made a new group for shapes.

--- PictureStory/Shapes/Ribbon.swift
+++ PictureStory/Shapes/Ribbon.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct Ribbon: Shape {
-   func path(in rect: CGRect) -> Path {
+    func path(in rect: CGRect) -> Path {
         Path { path in
             path.move(to: CGPoint(x: rect.minX, y: rect.minY))
             path.addLine(to: CGPoint(x: rect.maxX/7*1, y: rect.maxY/15*1))
@@ -18,9 +18,9 @@ struct Ribbon: Shape {
             path.addLine(to: CGPoint(x: rect.maxX/7*6, y: rect.maxY/15*1))
             path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
             path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-       }
+        }
     }
-    }
+}
 #Preview {
     Ribbon()
 }
